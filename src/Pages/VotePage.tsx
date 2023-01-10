40bec37f77331fa2ff10e9d061e51a345252d4b6
Didier Osindero <didierosindero@gmail.com>
2023-01-10 15:50:49 +0000

    Fetch dog data

--- src/Pages/VotePage.tsx
+++ src/Pages/VotePage.tsx
@@ -1,13 +1,35 @@
 import axios from "axios";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { IDogDataWithBreed } from "../interfaces";
+import { getDogBreed } from "../utils/getDogBreed";
 
 export function VotePage(): JSX.Element {
-  async function getDogImage() {
+  const [dogDataArray, setDogDataArray] = useState<IDogDataWithBreed[]>([]);
+
+  async function getDogData() {
     const { data } = await axios.get("https://dog.ceo/api/breeds/image/random");
+    console.log("data", data)
     return data;
   }
+
+  const fetchBothDogs = useCallback(async () => {
+    const firstDog = getDogBreed(await getDogData());
+
+    //While the second dog is the same breed as the first dog, continue fetching
+    let secondDog: IDogDataWithBreed  =  getDogBreed(await getDogData());
+    while (firstDog.breed === secondDog.breed && firstDog.subBreed === secondDog.subBreed) {
+       secondDog  = await getDogData();
+    }
+    
+    setDogDataArray([firstDog, secondDog])
+  }, [])
+ 
+  useEffect(() => {
+    fetchBothDogs();
+  }, [fetchBothDogs])
+  console.log("dogDataArray", dogDataArray)
 
   return <>Vote Page</>;
 }
 
-function handleVoteClick() {}
+
