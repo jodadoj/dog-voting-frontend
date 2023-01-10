3774fd0d8c3c583a93f2f656c4cfcebe8a1bcc51
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-10 13:17:22 +0000

    Handing over

--- src/Pages/VotePage.tsx
+++ src/Pages/VotePage.tsx
@@ -1,6 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
 
 
 export function VotePage():JSX.Element{
+
+    async function getDogImage(){
+        const {data} = await axios.get("https://dog.ceo/api/breeds/image/random");
+        return data;
+    }
 
     return (
         <>
---
+++ src/utils/getDogBreed.ts
@@ -0,0 +1,12 @@
+import { IDogDataFromAPI, IDogDataWithBreed } from "../interfaces";
+
+    export function getDogBreed(DogData:IDogDataFromAPI):IDogDataWithBreed{
+        
+        const imgURL = DogData.message;
+
+        return 
+    }
+
+    // "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_197.jpg"
+    // "https://images.dog.ceo/breeds/leonberg/n02111129_3045.jpg",
+// 
