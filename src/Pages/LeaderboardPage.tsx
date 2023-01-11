196b254de587d43b2fcf449c26e3d00439dcdc85
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 10:36:52 +0000

    LeaderboardPage, fetch top 10 dogs

--- src/Pages/LeaderboardPage.tsx
+++ src/Pages/LeaderboardPage.tsx
@@ -1,3 +1,29 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { baseUrl } from "../App";
+import { ILeaderboard } from "../interfaces";
+
 export function LeaderboardPage(): JSX.Element {
-  return <>Leaderboard Page</>;
+  const [top10Doggies, setTop10Doggies] = useState<ILeaderboard[]>();
+
+  const fetchTop10Doggies = useCallback(async () => {
+    try {
+      const { data } = await axios.get(baseUrl + "leaderboard");
+      setTop10Doggies(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [setTop10Doggies]);
+
+  useEffect(() => {
+    fetchTop10Doggies();
+  }, [fetchTop10Doggies]);
+
+  return (
+    <div className="ctn-leaderboard-page">
+      <div className="ctn-leaderboard">
+        <p>Top 10</p>
+      </div>
+    </div>
+  );
 }
