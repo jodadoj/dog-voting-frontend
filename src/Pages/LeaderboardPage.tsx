480b0a1f4978065ea1f66b7c20da6f84fed20982
Didier Osindero <didierosindero@gmail.com>
2023-01-11 16:07:04 +0000

    CSS for leaderboard

--- src/Pages/LeaderboardPage.tsx
+++ src/Pages/LeaderboardPage.tsx
@@ -25,17 +25,21 @@ export function LeaderboardPage(): JSX.Element {
       <button className="btn-refresh-leaderboard" onClick={fetchTop10Doggies}>
         refresh leaderboard
       </button>
-      <ol className="ctn-leaderboard">
+      <div className="ctn-leaderboard">
+      <div className="ctn-leaderboard-row" >
+                <div className="ctn-leaderboard-row-breed"><b>Dog</b></div>
+                <div className="ctn-leaderboard-row-votes"><b>Votes</b></div>
+              </div>
         {top10Doggies.length > 0 &&
           top10Doggies.map((oneDog, index) => {
             return (
-              <li className="ctn-leaderboard-row" key={oneDog.breed}>
+              <div className="ctn-leaderboard-row" key={oneDog.breed}>
                 <div className="ctn-leaderboard-row-breed">{oneDog.breed}</div>
                 <div className="ctn-leaderboard-row-votes">{oneDog.votes}</div>
-              </li>
+              </div>
             );
           })}
-      </ol>
+      </div>
     </div>
   );
 }
