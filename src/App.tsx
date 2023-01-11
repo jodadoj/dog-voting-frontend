22e72a26e0883debe53a244be6e90ebcb0adedc7
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-11 13:20:46 +0000

    Working Nav element

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LeaderboardPage } from "./Pages/LeaderboardPage";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-//--------------------------------------------------------------------------------the definition of the base URL
+//-------------------------------------------------------------------------------the definition of the base URL
 
 export const baseUrl: string =
   process.env.NODE_ENV === "production"
@@ -16,11 +16,28 @@ export const baseUrl: string =
 //   const randomImageURL = "https://dog.ceo/api/breeds/image/random"
 // }
 
+//-------------------------------------------------------------------------------Main content
 function App(): JSX.Element {
   return (
     <div className="app">
+
+{/* //---------------------------------------------------------------------------Header */}
       <h1>Dog Voting App</h1>
-      <nav className="navbar"></nav>
+
+{/* //---------------------------------------------------------------------------Navbar */}
+
+      <nav className="navbar">
+        <Link to={"/"}>
+          Vote 
+        </Link>
+        <Link to={"/leaderboard"}>
+          Leaderboard 
+        </Link>
+        {/* Add Links here */}
+      </nav>
+
+{/* //---------------------------------------------------------------------------Routing */}
+
       <Routes>
         <Route path="/" element={<VotePage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
